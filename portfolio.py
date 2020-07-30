ea442ce4700ae59fc0d602d3271b3d732715fb2e
Swathi Chitta <chittaswathi98@gmail.com>
2020-07-30 06:34:57 +0530

    Update portfolio.py
    Commenting out unnecessary code

--- portfolio.py
+++ portfolio.py
@@ -1,27 +1,27 @@
 from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
-print('app', app)
+# print('app', app)
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/projects/')
-@app.route('/projects/<name>')
-def projects(name=None):
-    if not name:
-        return render_template('projects.html')
-    else:
-        return render_template('project_details.html', name=name)
+# @app.route('/projects/')
+# @app.route('/projects/<name>')
+# def projects(name=None):
+#     if not name:
+#         return render_template('projects.html')
+#     else:
+#         return render_template('project_details.html', name=name)
 
-# with app.test_request_context():
-#     print(url_for('projects'))
+# # with app.test_request_context():
+# #     print(url_for('projects'))
 
 
 
 
 if __name__ == '__main__':
-    print('app in main', app)
+#     print('app in main', app)
     app.run()
